37a1b3b5d96ce98d4c2603911dddc8416607ed13
junghwaK <jeonghwa054@gmail.com>
2022-09-20 12:46:28 +0900

    study: handling differentr routes

--- 04-limiting-middleware-execution-to-post-requests/app.js
+++ 04-limiting-middleware-execution-to-post-requests/app.js
@@ -3,22 +3,16 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser. urlencoded({extended: false}));
-
-// app.use('/', (req, res, next) => {
-//     console.log('This always runs!');
-//     next();
-// });
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/add-product', (req, res, next) => {
-  // console.log('In another middleware!');
   res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
 
-app.use('/product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-})
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
 
 app.use('/', (req, res, next) => {
   res.send('<h1>Hello from Express!</h1>');
