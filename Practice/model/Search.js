15c6e7b1f8490d8bf306729a987bb5a731814814
junghwaK <jeonghwa054@gmail.com>
2022-10-02 01:20:17 +0900

    project: justified model.js with dbtable

---
+++ Practice/model/Search.js
@@ -0,0 +1,27 @@
+const Search = (Sequelize, DataTypes) => {
+    const model = Sequelize.define('pr',{
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+            allowNull: false
+        },
+        name: {
+            type: DataTypes.STRING(225),
+            allowNull: false
+        },
+        symbol: {
+            type: DataTypes.STRING(225),
+            allowNull: false
+        }
+    },
+    {
+        timestamps: false,
+        tableName: 'pr',
+        freeseTableName: true
+    }
+    );
+    return model;
+}
+
+module.export = Search;
