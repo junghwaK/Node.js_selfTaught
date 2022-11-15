c5d92297a52ecb5e5d18602d45c0fd16a58c2268
junghwaK <jeonghwa054@gmail.com>
2022-11-15 18:47:52 +0900

    JS: const and let

--- Javascript/play.js
+++ Javascript/play.js
@@ -1,7 +1,21 @@
-var name = 'Jay';
-var age = 29;
+// var name = 'Jay';
+// var age = 29;
+// console.log(name);
+// var hasHobbeies = true;
+
+// function summerizeUser(userName, userAge, userHasHobby){
+//     return ('Name is' + userName + 'age is' + userAge + 'and the user has hobbies : ' +
+//     userHasHobby);
+// }
+
+// console.log(summerizeUser(name, age, hasHobbeies));
+
+//const, let
+let name = 'Jay';
+let age = 29;
 console.log(name);
-var hasHobbeies = true;
+let hasHobbeies = true;
+
 
 function summerizeUser(userName, userAge, userHasHobby){
     return ('Name is' + userName + 'age is' + userAge + 'and the user has hobbies : ' +
